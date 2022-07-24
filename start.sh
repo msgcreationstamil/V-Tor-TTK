16778551915d8c5c45b56eb5ff049436521fc709
msgcreations <86725442+MSGCreation@users.noreply.github.com>
2022-07-24 21:47:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,18 @@
-# DOWNLOAD and Update QBT trackers on Each Reboot
-qbt_trackers_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_exclude.txt | awk '$0' | tr '\n' ',')
-
-echo "😉Downloading and Updating custom trackers for QBT 🧲🧲"
-echo "Bittorrent\add_trackers=$qbt_trackers_list" >> .config/qBittorrent/qBittorrent.conf
-#
+hour=0
+ min=0
+ sec=10
+        while [ $hour -ge 0 ]; do
+                 while [ $min -ge 0 ]; do
+                         while [ $sec -ge 0 ]; do
+                                 echo -ne "$hour:$min:$sec\033[0K\r"
+                                 let "sec=sec-1"
+                                 sleep 1
+                         done
+                         sec=10
+                         let "min=min-1"
+                 done
+                 min=0
+                 let "hour=hour-1"
+         done
 
 ./alive.sh & gunicorn tortoolkit:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & qbittorrent-nox -d & python3 -m tortoolkit
